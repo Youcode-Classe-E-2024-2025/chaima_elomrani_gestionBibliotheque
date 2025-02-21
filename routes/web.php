629884355015ca51b8ff33076a626eb0dcc85ae3
chaima-syc <chaimaelomrani6@gmail.com>
2025-02-21 01:51:21 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\HomeController;
 Route::get('register', [AuthController::class, 'showRegisterForm']);
 Route::post('register', [AuthController::class, 'register']);
 Route::get('login', [AuthController::class, 'showLoginForm']);
+Route::post('login', [AuthController::class, 'Login']);
 Route::get('/profile', [AuthController::class, 'profile']);
-Route::get('/profile', [AuthController::class, 'profile']);
+// Route::get('/profile', [AuthController::class, 'profile']);
 Route::get('/', [HomeController::class, 'index']);
 
 
